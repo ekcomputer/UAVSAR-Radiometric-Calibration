67e6697a738eae394ffc735e1e2ba6d568270d7f
ek-adapt <ethan_kyzivat@brown.edu>
2020-06-01 11:25:51 -0400

    add

---
+++ python/TODO.md
@@ -0,0 +1,29 @@
+List of changes to be made:
+
+* add multiprocessing to increase speed, or vectorize LUT process in radiocal.py
+* add auto no-overwrite
+* add auto gdal warp to extract landcover mask raster
+* add cropping procedure into workflow?
+* add auto min/max look angle masking?
+* **set min/max_look and min_samples to dynamically update based on image size and look geom**
+* import 2019 lines from local
+
+Switch for real run:
+
+* min and max looks
+* flat DEM = false
+* run for all three polarizations
+* keep min_samples high ca 10000
+* run over all 6 input files or just main 3?
+* Check that LUT has non-zero edges for my files
+
+Done:
+
+* rename _slope.grd -> .slope
+* Change 'allowed' back to all 14 classes
+* add defensive checks for improper file naming or missing files
+
+For pull request:
+* include LUT folder
+* new build_lut() vectorized version
+* parallel?
---
+++ python/slurm_radiocal_example_script.sh
@@ -0,0 +1,29 @@
+#!/usr/local/bin/bash
+
+# #SBATCH -e ~/slurm-logs/radiocal_example_script.py.%J.err # -e is stderr
+# #SBATCH -o ~/slurm-logs/radiocal_example_script.py.%J.out # -o is stdout
+#SBATCH --job-name=radiocal_workflow
+#SBATCH --mem-per-cpu=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --export=ALL
+
+## Reporting  start #############################
+start_time="$(date -u +%s)"
+echo "  Job: $SLURM_ARRAY_JOB_ID"
+echo
+echo "  Started on:           " `/bin/hostname -s`
+echo "  Started at:           " `/bin/date`
+#################################################
+
+source activate base
+
+# srun allows printing in real time?
+python /home/ekyzivat/scripts/UAVSAR-Radiometric-Calibration-fork/python/radiocal_example_script_ek.py
+
+## Reporting stop ###############################
+echo "  Finished at:           " `date`
+end_time="$(date -u +%s)"
+elapsed="$(($end_time-$start_time))"
+echo "  Minutes elapsed:       " $(($elapsed / 60))
+echo
+#################################################
